4a90ab0ea7057869eec067f6f34da7a3b9809b47
jarrinson parababi <jarrinsonparababi531@gmail.com>
2023-01-11 14:59:08 -0400

    Jarrinson/tienda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tienda';
+  public ArregloTarjetas:Tarjeta[]=[]
+
+  ngOnInit(): void {
+    this.ArregloTarjetas = [
+      {titulo:'video 1', subtitulo: 'subtitulo video 1'},
+      {titulo:'video 2', subtitulo: 'subtitulo video 2'},
+      {titulo:'video 3', subtitulo: 'subtitulo video 3'},
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidebarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
